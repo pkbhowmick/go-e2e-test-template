f7eedd4c1874f17cd56ff5f7db0baa9b4a1a9217
Pulak Kanti Bhowmick <pkbhowmick007@gmail.com>
2023-08-05 00:53:07 +0600

    Add postman+newman based test
    Signed-off-by: Pulak Kanti Bhowmick <pkbhowmick007@gmail.com>

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,11 +14,15 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
 	r.Get("/hello", helloHandler)
 
 	srv := &http.Server{
-		Addr:    "8080",
+		Addr:    "0.0.0.0:8080",
 		Handler: r,
 	}
-	srv.ListenAndServe()
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
 }
